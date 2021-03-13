8e12ac5690eab812f0015a5be028fcb8918695dd
rry <r.yamakuchi@gmail.com>
2021-03-13 10:16:45 +0900

    add: Chapter5（途中まで）

---
+++ src/components/Chapter5.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>CH5 コンポーネントで UI 部品を作る</h1>
+  <h2>コンポーネント間の通信 / 親から子</h2>
+  <CompChild val="これは子 A" />
+  <CompChild :val="'これは子 B'" />
+
+  <h2>コンポーネントをリストレンダリング</h2>
+  <CompChild @childsEvent="parentsMethod()" />
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from 'vue'
+import CompChild from './CompChild.vue'
+
+defineComponent({
+  name: 'Chapter5',
+  components: {
+    CompChild,
+  },
+})
+
+const parentsMethod = (): void => alert('イベントをキャッチ！')
+</script>
