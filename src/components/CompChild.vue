16af93ddd943369a5f8a5abcba9e4af210b23046
Nahoko Matsui <nahokomatsui8213@gmail.com>
2021-03-14 01:35:57 +0900

    fix: defineProps, defineEmit を使って CompChild を script setup 形式で書き換え

--- src/components/CompChild.vue
+++ src/components/CompChild.vue
@@ -3,23 +3,19 @@
   <button v-if="!val" @click="handleClick()">イベント発火</button>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
+import { defineProps, defineEmit } from 'vue'
 
-export default defineComponent({
-  props: {
-    val: {
-      type: String,
-      default: '',
-    },
-  },
-  emits: ['childsEvent'],
-  setup(_, context) {
-    return {
-      handleClick: () => {
-        context.emit('childsEvent')
-      }
-    }
+defineProps({
+  val: {
+    type: String,
+    default: '',
   },
 })
+
+const emit = defineEmit(['childsEvent'])
+
+const handleClick = () => {
+  emit('childsEvent')
+}
 </script>
